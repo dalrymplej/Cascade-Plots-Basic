6e412c78d105df85bd068f9dcddb455aed26f767
marathonman4202 <roy.haggerty@oregonstate.edu>
2014-10-16 22:19:45 -0700

    matrix_from_xls added
    should read in xls column and transfer it to a matrix of width xcycles
    beginning at data point skip

--- matrix_from_xls.py
+++ matrix_from_xls.py
@@ -1,6 +1,12 @@
-def matrix_from_xls(file_w_path,column):
+def matrix_from_xls(file_w_path,column,xcycle,skip):
     """Reads xls file and produces 2-D matrix
     """
     import numpy as np
     
     data_tmp = np.array(np.genfromtxt(file_w_path, delimiter=',',skip_header=1)) # Read csv file
+    data_yr_tmp = data_tmp[:,column]
+    numdat = len(data_yr_tmp)
+    data_yr = data_yr_tmp[skip:-(xcycle-skip+numdat%xcycle)] # start at skip
+    data_2D = np.reshape(np.array(data_yr), (-1,xcycle)) #2D matrix of data in numpy format
+    
+    return data_2D
